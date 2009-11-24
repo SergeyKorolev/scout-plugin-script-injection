b7ad4c08d174732ae7bc33c9d8a7a7861bfa92e7
Sergey Korolev <sergey.korolev@railsware.com>
2009-11-24 18:02:53 +0200

    support lang

--- plugin.rb
+++ plugin.rb
@@ -1,16 +1,26 @@
+ 
 class ScriptExecuter < Scout::Plugin
 
 def build_report
   value = 0
   
-  exec_command = option("exec_command")
+  script_lang = option("script_lang").downcase
+  exec_script = option("exec_script")
   value_sym = option("value_name").to_sym  
-  value = `#{exec_command}`.strip
-
+  
+  if script_lang == "shell"
+    value = `#{exec_script}`.strip
+  elseif script_lang == "ruby"
+    value = eval(exec_script).strip
+  else
+    error( :subject => "#{script_lang} is not supported", :body => "#{script_lang} is not supported. Supported languages are shell and ruby")
+    return 
+  end
+  
   report(value_sym => value)
   
   rescue Exception => e
-     error( :subject => "Oh no!", :body => "#{ e } (#{ e.class })!")  
+     error( :subject => "Exception appeared", :body => "#{ e } (#{ e.class })!")  
 end
 
 end
