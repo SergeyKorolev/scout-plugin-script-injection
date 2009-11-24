4c909ad19ffe3d9224fb517ba7c4ab0104206ba1
Sergey Korolev <sergey.korolev@railsware.com>
2009-11-24 17:17:33 +0200

    first run test

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,16 @@
 class ScriptExecuter < Scout::Plugin
 
 def build_report
-  report( :my_size => rand(200) )
+  value = 0
+  
+  exec_command = option("exec_command")
+  value_sym = option("value_name").to_sym  
+  value = `#{exec_command}`.strip
 
-  rescue
-     error( :subject => "Error talking to Amazon SQS", :body => "Oh no!")  
+  report(value_sym => value)
+  
+  rescue Exception => e
+     error( :subject => "Oh no!", :body => "#{ e } (#{ e.class })!")  
 end
 
 end
